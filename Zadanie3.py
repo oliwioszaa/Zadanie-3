6da9b786d423278bb8ad012484f260b2fab790b4
oliwioszaa <115988763+oliwioszaa@users.noreply.github.com>
2023-12-02 13:36:38 +0100

    Add files via upload

--- Zadanie3.py
+++ Zadanie3.py
@@ -3,8 +3,15 @@ liczba_tablicy = int(input("Podaj liczbę tablicy: "))
 max_liczba = int(input("Podaj maksymalną liczbę do wygenerowania: "))
 
 tablica = list(range(1, max_liczba + 1))
+roznica = max_liczba - liczba_tablicy
 
-print(f"Tablica {liczba_tablicy}: {tablica}")
+if max_liczba > liczba_tablicy:
+    tablica = list(range(1, max_liczba))
+
+if max_liczba > liczba_tablicy:
+    print(f"Tablica {liczba_tablicy}: {tablica}")
+else:
+    print(f"Tablica {liczba_tablicy}: {tablica}")
 
 with open("tablica.txt", "w") as file:
     print(f"Tablica {liczba_tablicy}: {tablica}", file=file)
